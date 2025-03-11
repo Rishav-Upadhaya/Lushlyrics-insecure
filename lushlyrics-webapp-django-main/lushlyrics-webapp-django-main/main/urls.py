ec23976e197de685f3885e6f05df043479300343
Rishav-Upadhaya <rishavupadhaya266@gmail.com>
2025-03-11 15:52:23 +0545

    Added user authentication and password recovery

---
+++ lushlyrics-webapp-django-main/lushlyrics-webapp-django-main/main/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path("", views.default, name='default'),
+    path("playlist/", views.playlist, name='your_playlists'),
+    path("search/", views.search, name='search_page'),
+    path("login", views.login_view, name='login'),
+    path("signup", views.signup_view, name='signup'),
+    path("logout", views.logout_view, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+]
